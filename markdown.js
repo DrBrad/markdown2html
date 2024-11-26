9210c611bda3be8827d3369b828f6fecdb4d59ba
DrBrad <brad@bradeagle.com>
2024-11-26 00:22:11 -0700

    Link checking validate

--- markdown.js
+++ markdown.js
@@ -253,9 +253,25 @@ function markDownText(line){
 
         if(startText !== -1 && endText !== -1 && startUrl !== -1 && endUrl !== -1){
             const url = line.substring(startUrl+1, endUrl);
-            if(url.includes('"')){
+            const pattern = new RegExp(
+                '^(' +
+                '([a-zA-Z]+:\\/\\/)?' + // protocol
+                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+                '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
+                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+                '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+                '(\\#[-a-z\\d_]*)?' + // fragment locator
+                '|' +
+                '(\\/[-a-z\\d%_.~+]+(\\/[-a-z\\d%_.~+]+)*\\/?)' + // relative path with optional trailing slash
+                '|' +
+                '(\\#[-a-z\\d_]*)' + // fragment link
+                ')$',
+            'i');
+
+            if(!pattern.test(url)){
                 break;
             }
+            
             const linkHtml = `<img src="${url}">`;
 
             line = line.slice(0, startText)+linkHtml+line.slice(endUrl+1);
